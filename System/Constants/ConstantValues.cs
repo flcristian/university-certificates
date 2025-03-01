0134b47a6bdf6b484cb7132a70627f2ff65d5c85
flcristian <qflorescucristian@gmail.com>
2025-03-01 22:25:57 +0200

    fix: deploy to include smtp and set wait time for certificate request to 0 for test phase

--- System/Constants/ConstantValues.cs
+++ System/Constants/ConstantValues.cs
@@ -3,5 +3,5 @@ namespace UniversityCertificates.System.Constants;
 public static class ConstantValues
 {
     public const int QR_CODE_SIZE = 96;
-    public const int REGISTER_ENTRY_WAIT_MINUTES = 60;
+    public const int REGISTER_ENTRY_WAIT_MINUTES = 0;
 }
